7c6ac6c933be13ac4ee0d09d7c487b8620d90c34
EzequielLopez <ezequiel.lopez.vallejos@gmail.com>
2024-10-24 17:50:37 -0300

    Uso de React Router y NavLink

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
+// Router
+import AppRouter from './router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppRouter />
   </StrictMode>,
 )
---
+++ src/views/FavoritesPage.tsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header"
+
+export default function FavoritesPage() {
+  return (
+    <>
+    <h1>Favoritos</h1>
+    </>
+  )
+}
